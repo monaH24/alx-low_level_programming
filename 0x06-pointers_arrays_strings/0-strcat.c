58426aab9a70adab900866ba4c8add2c6006983d
monaH24 <monahakim99@gmail.com>
2023-09-07 09:03:34 -0700

    Readme file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * _strcat(char *dest, char *src): concatenates two strings
+ */
+int main(void)
+{
+	char *_strcat(char *dest, char *src);
+	dest = return;
+}
