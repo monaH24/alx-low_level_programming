06553a034fe7105d378395bdf0fe9c2ac537378e
monaH24 <monahakim99@gmail.com>
2023-09-19 14:57:32 -0700

    testing~

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * set_string - sets the value of a pointer to a char
  *
- * Return: Always 0.
+ * @s: pointer to pointer to a char
+ *
+ * @to: the address to match
+ *
+ * Return: void
  */
-int main(void)
-{
-    char *s0 = "Anne Cognet";
-    char *s1 = "Asaia Palacios";
 
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
+void set_string(char **s, char *to)
+{
+	*s = to;
 }
