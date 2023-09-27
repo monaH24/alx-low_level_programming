69f246e067e4cfe9eb307709a0458c197c43df6e
monaH24 <monahakim99@gmail.com>
2023-09-27 07:16:04 -0700

    print listint2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	list_ i = 0;
-	
+	size_t i = 0;
+
 	while (h)
 	{
-		printf(%d\n,h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		++i;
 	}
-	return(i);
+	return (i);
 }
-
